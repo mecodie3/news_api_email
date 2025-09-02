aab571377844f10096769810b70be25ba6a86568
redroze <mecodie3@gmail.com>
2025-09-02 21:49:05 +0100

    Day 3: Add links,specify language,limit news,add subject,control category

--- main.py
+++ main.py
@@ -3,11 +3,15 @@
 import requests
 from send_email import send_email
 
+
+topic ="tesla"
 api_key="81eaadf604134ab297c47f8a80ea6bd3"
 
-url = "https://newsapi.org/v2/everything?q=tesla&"\
-    "from=2025-08-01&sortBy=publishedAt&apiKey=" \
-    "81eaadf604134ab297c47f8a80ea6bd3"
+url = "https://newsapi.org/v2/everything?" \
+    f"q={topic}&"\
+    "from=2025-08-01&"\
+      "sortBy=publishedAt&"\
+      "apiKey=81eaadf604134ab297c47f8a80ea6bd3&language=en"
 
 #Make request
 request =requests.get(url)
@@ -15,12 +19,19 @@ request =requests.get(url)
 #Get a dictionary with date
 content = request.json()
 
-#Access the article title and description
-body= ""
-for article in content["articles"]:
-    if article["title"] is not None:
-        body = body + article["title"]+ "\n"+ article["description"]+2*"\n"
+#Access the article title and description, articles are limited to 20
+body = "Subject: Today's news\n\n"
 
+for article in content["articles"][:20]:
+    title = article.get("title", "")
+    description = article.get("description", "")
+    url = article.get("url", "")
+
+    body += (
+        f"{title}\n"
+        f"{description}\n"
+        f"{url}\n\n"
+    )
 #correct the error
 body=body.encode("utf-8")
 send_email(message=body)
